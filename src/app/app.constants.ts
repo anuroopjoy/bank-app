580771777a37c18440158b097b5532da993c9989
Anuroop <anuroopjoy@gmail.com>
2021-11-26 23:28:08 +0530

    changes

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,9 +3,9 @@ import { DepositAccount } from './deposit-account/deposit-account';
 import { CompoundInterestCalculator } from './interest-calculator/compound-interest-calculator';
 import { InterestCalculator } from './interest-calculator/interest-calculator';
 
-export const BankMap = ['Savings', 'Deposit', 'Checking'];
+export const BankMap = ['Savings', 'Deposit'];
 
 export const EntityMap = new Map<string, BankAccount>([
-  ['Savings', new BankAccount(new CompoundInterestCalculator())],
+  ['Savings', new BankAccount(new InterestCalculator())],
   ['Deposit', new DepositAccount(new CompoundInterestCalculator())],
 ]);
--- src/app/bank-account/bank-account.ts
+++ src/app/bank-account/bank-account.ts
@@ -2,12 +2,12 @@ import { InterestCalculator } from '../interest-calculator/interest-calculator';
 import { IInterestCalculator } from '../interest-calculator/interest-calculator.interface';
 import { IBankAccount } from './bank-account.interface';
 
-export class BankAccount implements IBankAccount {
+export class BankAccount implements IBankAccount, IInterestCalculator {
   constructor(interestCalculator: IInterestCalculator) {
     this.interestCalculator = interestCalculator;
   }
-  interestCalculator!: IInterestCalculator;
-  amount = 0;
+  interestCalculator: IInterestCalculator;
+  amount = 1000;
   deposit(amount: number): void {
     this.amount += amount;
   }
